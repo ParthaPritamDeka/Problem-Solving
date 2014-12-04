2e18d06924795b04230aa4d4590603dbcd9a495b
ParthaPritamDeka <partha.pritamdeka@gmail.com>
2014-12-04 08:32:21 -0800

    Create Anagram_two_strings_without_sort.py

---
+++ Anagram_two_strings_without_sort.py
@@ -0,0 +1,29 @@
+def string_anagram(s1, s2):
+
+    dict_1 = {}
+    dict_2 = {}
+
+    for i in range(len(s1)):
+        if s1[i] not in dict_1:
+            dict_1[s1[i]] = 1
+
+        else:
+            dict_1[s1[i]] +=1
+
+    for j in range(len(s2)):
+        if s2[j] not in dict_2:
+            dict_2[s2[j]] = 1
+
+        else:
+            dict_2[s2[j]] +=1
+
+    print dict_1
+    print dict_2
+
+    if dict_1 == dict_2:
+        return True
+
+    return False
+
+
+print string_anagram('paaarthhhhhaaaa', 'thhhhhpaaraaaa')
