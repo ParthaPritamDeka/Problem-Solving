6000fa87bf7263f2c71cbbc65c33053d08fb59e1
Partha Pritam Deka <partha.pritamdeka@gmail.com>
2017-04-25 14:17:15 -0700

    update binary.py

--- binary.py
+++ binary.py
@@ -10,8 +10,31 @@ elements are in a strictly increasing order.
 Return the index of value, or -1 if the value
 doesn't exist in the list."""
 
-def binary_search(input_array, value):
+def binary_search(array, value):
     """Your code goes here."""
+    lower = 0
+    upper = len(array)
+    
+    while lower < upper:
+        
+        x = lower + (upper - lower) /2
+        print "this teration"
+        print x
+        
+        val = array[x]
+        
+        if target == val:
+            return x
+        
+        elif target > val:
+            if lower == x:
+                return -1
+            
+            lower = x
+            
+        else:
+            upper = x
+    return -1
     return -1
 
 test_list = [1,3,9,11,15,19,29]
