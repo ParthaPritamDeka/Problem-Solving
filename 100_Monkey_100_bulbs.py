0ff737cc73ced827e492769f0aa27969439c414d
Partha Pritam Deka <partha.pritamdeka@gmail.com>
2017-05-26 19:11:43 -0700

    100 Monkey 100 bulbs
    100 monkeys turning on/ off 100 bulbs skipping one one at a time 
    1st monkey turn on all bulbs, 2nd monkey flips every other bulb starting with 2nd, 3rd monkey flips every other bulb starting with 3rd  so on and so forth...... how bulbs are lighten after all monkeys are done with it

---
+++ 100_Monkey_100_bulbs.py
@@ -0,0 +1,38 @@
+# 100 monkeys turning off/ on 100 bulbs skipping one one at a time 
+
+num_monkeys =100
+
+n = num_monkeys + 1
+
+switch_state = n*[0]
+
+Monkey_list = n*[1]
+
+for i in range(len(Monkey_list)):
+    i = i + 1
+    if i == 1:
+        for j in range(len(switch_state)):
+            j = j + 1
+            if j > n-1:
+                break
+            switch_state[j] = 1
+        #print switch_state[1:n]
+    
+    else:
+        for j in range(i,len(switch_state),2):
+            #print j
+            if j > n-1:
+                break
+            if switch_state[j] == 0:
+                switch_state[j] = 1
+            else:
+                switch_state[j] = 0
+        #print ' '
+        #print switch_state[1:n]
+                
+#print switch_state
+        
+print len([x for x in switch_state[1:n] if x == 1])
+    
+
+    
