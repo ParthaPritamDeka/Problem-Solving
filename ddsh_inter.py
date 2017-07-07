58ed12b19ed9b50b78757fb9258e597357c851a9
Partha Pritam Deka <partha.pritamdeka@gmail.com>
2017-07-07 16:58:09 -0700

    ddsh_inter.py

---
+++ ddsh_inter.py
@@ -0,0 +1,31 @@
+import random
+def get_random_item(choices):
+    
+    #random_index = randrange(0, len(pr))
+    #if pr[random_index] <= 0.1:
+    
+    #choices ={ 0:0.1, 1:0.1, 2:0.8}
+    
+    space = {}
+    current = 0
+    
+    for choice , wt in choices.iteritems():
+        if wt > 0:
+            space[current] = choice
+            current  += wt
+    rand = random.uniform(0, current)
+    
+    for key in sorted(space.keys() + [current]):
+        if rand < key:
+            return choice
+        
+ #get_random_item({ 0:0.1, 1:0.1, 2:0.8})       
+    
+      #return int
+
+sample_dict = {0:0, 1:0, 2:0}
+for i in range(0, 100):
+    a = get_random_item({0:0.1, 1:0.1, 2:0.8})
+    if a in sample_dict:
+        sample_dict[a] += 1
+print sample_dict
