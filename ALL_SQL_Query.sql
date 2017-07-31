d6ad998a1261c9f38cf6ff14de053949e89d6c2c
Partha Pritam Deka <partha.pritamdeka@gmail.com>
2017-07-31 10:55:23 -0700

    Update ALL_SQL_Query.sql

--- ALL_SQL_Query.sql
+++ ALL_SQL_Query.sql
@@ -191,6 +191,19 @@ where b.ID > a.ID)
 
 /********************** PSOTGRESQL -Consecutive Ones Query ***********************/
 
+
+Id      Num  result
+1       1       0
+2       1       1
+3       1       1
+4       2       1
+5       2       0
+6       1       1
+7       1       0
+8       2       0
+9       2       0
+10      2       0
+
 select b.Num
 from (select a.Num, a.grp, count(*) as cnt 
       from (select Num, Id, (Id - row_number() over (partition by Num order by Id)) grp
