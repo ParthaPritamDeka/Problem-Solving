61f1b2e2b4b0403f569448f6a1550c2e63ad0cf9
Partha Pritam Deka <partha.pritamdeka@gmail.com>
2018-01-21 10:04:46 -0800

    Update ALL_SQL_Query.sql

--- ALL_SQL_Query.sql
+++ ALL_SQL_Query.sql
@@ -244,3 +244,62 @@ where   a.Id = b.Id - 1
     and b.Id = c.Id - 1
     and a.Num = b.Num
     and b.Num = c.Num
+
+
+/************** Human Traffic of Stadium***************/
+
+X city built a new stadium, each day many people visit it and the stats are saved as these columns: id, date, people
+
+Please write a query to display the records which have 3 or more consecutive rows and the amount of people more than 100(inclusive).
+
+For example, the table stadium:
++------+------------+-----------+
+| id   | date       | people    |
++------+------------+-----------+
+| 1    | 2017-01-01 | 10        |
+| 2    | 2017-01-02 | 109       |
+| 3    | 2017-01-03 | 150       |
+| 4    | 2017-01-04 | 99        |
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-08 | 188       |
++------+------------+-----------+
+For the sample data above, the output is:
+
++------+------------+-----------+
+| id   | date       | people    |
++------+------------+-----------+
+| 5    | 2017-01-05 | 145       |
+| 6    | 2017-01-06 | 1455      |
+| 7    | 2017-01-07 | 199       |
+| 8    | 2017-01-08 | 188       |
++------+------------+-----------+
+
+Create table If Not Exists stadium (id int, date DATE NULL, people int);
+Truncate table stadium;
+insert into stadium (id, date, people) values ('1', '2017-01-01', '10');
+insert into stadium (id, date, people) values ('2', '2017-01-02', '109');
+insert into stadium (id, date, people) values ('3', '2017-01-03', '150');
+insert into stadium (id, date, people) values ('4', '2017-01-04', '99');
+insert into stadium (id, date, people) values ('5', '2017-01-05', '145');
+insert into stadium (id, date, people) values ('6', '2017-01-06', '1455');
+insert into stadium (id, date, people) values ('7', '2017-01-07', '199');
+insert into stadium (id, date, people) values ('8', '2017-01-08', '188');
+
+WITH people_grp as (
+select id, date, people,
+       (extract(epoch from date)::int /86400 - 
+        row_number() over(order by date)) grp
+from stadium
+where people> 100)
+select people_grp.id, people_grp.date, people_grp.people
+from(select grp, (max(date)-min(date))+1 as cnt
+from people_grp
+group by grp)x
+join people_grp 
+on x.grp = people_grp.grp
+and x.cnt >=3
+order by people_grp.id, people_grp.date
+
+/************** Human Traffic of Stadium***************/
